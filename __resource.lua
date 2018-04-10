7d7478b54acb0eb2ada516fc18bf0b4006cf467a
renildomarcio <krsolucoesweb@gmail.com>
2018-04-10 03:27:54 -0300

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,10 +1,11 @@
 description 'Skin Changer'
 
-version '1.0.1'
+version '1.0.2'
 
 client_scripts {
   'locale.lua',
   'locales/de.lua',
+  'locales/br.lua',
   'locales/en.lua',
   'locales/fr.lua',
   'config.lua',
